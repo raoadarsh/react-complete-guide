501a6eaf1bab249b2735c2d7aba5c06c9571cb1e
raoadarsh <raoadarsh@gmail.com>
2019-09-21 13:29:09 +0530

    SideDrawer completion

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './BuildControl.css'
+
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <label className={classes.Label}>{props.label}</label>
+        <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
+        <button className={classes.More} onClick={props.added}>More</button>
+    </div>
+);
+
+// const buildControl = (props) => (
+//     <div className={classes.BuildControl}>
+//         <label className={classes.Label}>{props.label}</label>
+//         <button className={classes.Less}>Less</button>
+//         <button className={classes.More}>More</button>
+//     </div>
+// );
+
+
+
+export default buildControl;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Aux from '../../hoc/Auxiliary';
+import Toolbar from'../Navigation/Toolbar/Toolbar';
+import classes from './Layout.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+const Layout = props => (
+    <Aux>
+    <Toolbar/>
+    <SideDrawer/>
+    <main className={classes.Content}>{props.children}</main>
+    </Aux>
+  );
+
+export default Layout;
